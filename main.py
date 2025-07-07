901b0bafc845d672b034da25118317258ecdb93c
Aarush528 <aajha1009@gmail.com>
2025-07-07 14:00:59 +0530

    Create main.py
    Added user input for subject choice

---
+++ main.py
@@ -0,0 +1,9 @@
+main.py
+# main.py
+# Student Learning AI (Starter backend)
+print("Welcome to Student Learning AI - Project by Aarush Jha")
+
+name = input("Enter your name: ")
+subject = input("What subject do you need help with? ")
+
+print(f"Hi {name}! Let's find resources for {subject}.")
